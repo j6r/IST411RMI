84b32c5d46cf1e97a838aaddddda8b1f07471ac2
j6r <32883148+j6r@users.noreply.github.com>
2018-03-29 18:30:10 -0700

    Use /dist instead of /output/dist

--- run_server.sh
+++ run_server.sh
@@ -13,19 +13,19 @@ pkill rmiregistry
 
 echo "Resetting\n"
 export PROJECT_DIR=`pwd`
-rm -rf $PROJECT_DIR/output
-mkdir $PROJECT_DIR/output
+rm -rf $PROJECT_DIR/dist
+mkdir $PROJECT_DIR/dist
 
 echo "Building engine\n"
 mvn clean package -q
 
 echo "Starting rmiregistry\n"
-cd $PROJECT_DIR/output/dist
+cd $PROJECT_DIR/dist
 rmiregistry &
 
 echo "Starting engine\n"
 java -cp $PROJECT_DIR/compute/target/compute-1.SNAPSHOT.jar \
-    -Djava.rmi.server.codebase=file:$PROJECT_DIR/output/dist/ \
+    -Djava.rmi.server.codebase=file:$PROJECT_DIR/dist/ \
     -Djava.rmi.server.hostname=127.0.0.1 \
     -Djava.security.policy=$PROJECT_DIR/server.policy \
     -jar $PROJECT_DIR/engine/target/engine-1.0-SNAPSHOT.jar
