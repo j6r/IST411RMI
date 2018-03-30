510463af3f2d877196994d4654e0fcf6d89e9e3f
j6r <32883148+j6r@users.noreply.github.com>
2018-03-29 18:27:24 -0700

    Refactored variables, use module jar locations in run_server

--- run_server.sh
+++ run_server.sh
@@ -12,22 +12,22 @@ trap "shutdown" 1
 pkill rmiregistry
 
 echo "Resetting\n"
-export PROJECT_DIR=`pwd`/output
-rm -rf $PROJECT_DIR
-mkdir $PROJECT_DIR
+export PROJECT_DIR=`pwd`
+rm -rf $PROJECT_DIR/output
+mkdir $PROJECT_DIR/output
 
 echo "Building engine\n"
 mvn clean package -q
 
 echo "Starting rmiregistry\n"
-cd $PROJECT_DIR/dist
+cd $PROJECT_DIR/output/dist
 rmiregistry &
 
 echo "Starting engine\n"
-java -cp $PROJECT_DIR/dist/compute-1.SNAPSHOT.jar \
-    -Djava.rmi.server.codebase=file:$PROJECT_DIR/dist/ \
+java -cp $PROJECT_DIR/compute/target/compute-1.SNAPSHOT.jar \
+    -Djava.rmi.server.codebase=file:$PROJECT_DIR/output/dist/ \
     -Djava.rmi.server.hostname=127.0.0.1 \
-    -Djava.security.policy=$HOME/Documents/Class/IST411_workspace/L09RMISolo/server.policy \
-    -jar $PROJECT_DIR/bin/engine-1.0-SNAPSHOT.jar
+    -Djava.security.policy=$PROJECT_DIR/server.policy \
+    -jar $PROJECT_DIR/engine/target/engine-1.0-SNAPSHOT.jar
 
 #   -Djava.security.debug=access,failure \
