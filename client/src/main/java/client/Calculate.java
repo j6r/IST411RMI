a0fda4127b666fb5f0f34532ab6c3166fd3811a3
j6r <32883148+j6r@users.noreply.github.com>
2018-03-29 17:43:31 -0700

    Better exception handling

--- client/src/main/java/client/Calculate.java
+++ client/src/main/java/client/Calculate.java
@@ -2,9 +2,14 @@ package client;
 
 import compute.Compute;
 import compute.Task;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.InputMismatchException;
 
 public class Calculate {
 
@@ -13,25 +18,26 @@ public class Calculate {
       if (System.getSecurityManager() == null) {
          System.setSecurityManager(new SecurityManager());
       }
+      
       try (Scanner scanner = new Scanner(System.in)) {
          String name = "Compute";
          Registry registry = LocateRegistry.getRegistry(0);
          Compute comp = (Compute) registry.lookup(name);
 
-         int operand1;
-         int operand2;
+         double operand1;
+         double operand2;
          String operator = "";
-         double result = 0.00;
+         double result;
          Task<Double> task;
 
-         System.out.println("Please enter the first operand: ");
-         operand1 = scanner.nextInt();
-
-         System.out.println("Please enter the second operand: ");
-         operand2 = scanner.nextInt();
-
-         System.out.println("Please enter the operator [+ - * /]: ");
+         System.out.println("Welcome to the calculator!");
+         System.out.println("You can enter calculations in the format 'n1 op n2'");
+         System.out.println("where n1 and n2 are doubles and op is one of the following mathematical operators: +,-,*,/.");
+         System.out.println("Please enter your calculation: ");
+                  
+         operand1 = scanner.nextDouble();
          operator = scanner.next();
+         operand2 = scanner.nextDouble();
 
          switch (operator) {
             case "+":
@@ -52,16 +58,17 @@ public class Calculate {
 
          if (task != null) {
             result = comp.executeTask(task);
-            System.out.printf("%d %s %d = %f%n",
+            System.out.printf("%f %s %f = %f%n",
                     operand1, operator, operand2, result);
          } else {
-            System.out.printf("Error: unknown operator %s%n", operator);
+            Logger.getLogger(Calculate.class.getName()).log(Level.SEVERE, 
+                    String.format("Error: unknown operator %s%n", operator));
          }
 
-      } catch (Exception e) {
-         System.err.println("Calculate exception:");
-         e.printStackTrace();
+      } catch (RemoteException|NotBoundException ex) {
+         Logger.getLogger(Calculate.class.getName()).log(Level.SEVERE, "Server error", ex);
+      } catch (InputMismatchException ex) {
+         Logger.getLogger(Calculate.class.getName()).log(Level.SEVERE, "Unable to parse calculation.", ex);
       }
-
    }
 }
