a7d75db9320506faaf60a3c3bb82318447218d0b
j6r <32883148+j6r@users.noreply.github.com>
2018-03-29 18:00:18 -0700

    Refactor variables and jars in run_client part 2

--- run_client.sh
+++ run_client.sh
@@ -2,9 +2,8 @@
 
 export PROJECT_DIR=`pwd`
 
-
-java -cp $PROJECT_DIR/compute/target/compute-1.SNAPSHOT.jar:$PROJECT_DIR/output/dist/:$PROJECT_DIR/client/target/client-1.0-SNAPSHOT.jar \
-     -Djava.rmi.server.codebase="file:$PROJECT_DIR/client/target/client-1.0-SNAPSHOT.jar file:$PROJECT_DIR/output/dist/" \
+java -cp $PROJECT_DIR/compute/target/compute-1.SNAPSHOT.jar:$PROJECT_DIR/client/target/client-1.0-SNAPSHOT.jar \
+     -Djava.rmi.server.codebase="file:$PROJECT_DIR/client/target/client-1.0-SNAPSHOT.jar" \
      -Djava.rmi.server.hostname=127.0.0.1 \
      -Djava.security.policy=$PROJECT_DIR/server.policy \
      -jar $PROJECT_DIR/client/target/client-1.0-SNAPSHOT.jar
